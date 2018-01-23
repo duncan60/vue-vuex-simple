434d8b7523bf04bbf61a0f3c51f6a84862b3d3f9
ducan60 <tmduncan60@gmail.com>
2018-01-23 17:57:00 +0800

    add fetch api unit testing

--- src/store/modules/fetch.js
+++ src/store/modules/fetch.js
@@ -10,14 +10,14 @@ const getters = {};
 const actions = {
   async fetchAPI({ commit }) {
     const res = await axios.get('https://api.github.com/');
-    commit(types.FETCH_API, res);
+    commit(types.FETCH_API, res.data);
   },
 };
 
 const mutations = {
   [types.FETCH_API](state, payload) {
     state.data = {
-      ...payload.data,
+      ...payload,
     };
   },
 };
--- test/unit/specs/store/modules/fetch.spec.js
+++ test/unit/specs/store/modules/fetch.spec.js
@@ -1,19 +1,58 @@
-// import fetchStore from '@/store/modules/fetch';
-// import * as types from '@/store/mutation-types';
-// import { testAction } from '../../../../test-action';
+import moxios from 'moxios';
+import fetchStore from '@/store/modules/fetch';
+import * as types from '@/store/mutation-types';
+import { testAction } from '../../../../test-action';
 
-// describe('fetch store - actions', () => {
-//   it('updateField', (done) => {
-//     testAction(
-//       fetchStore.actions.fetchAPI,
-//       [],
-//       {},
-//       [
-//         {
-//           type: types.FETCH_API,
-//         },
-//       ],
-//       done
-//     );
-//   });
-// });
+describe('fetch store - muations', () => {
+  it(types.FETCH_API, () => {
+    fetchStore.state = { data: {} };
+    fetchStore.mutations[types.FETCH_API](fetchStore.state, {
+      url: 'https://www.google.com.tw/',
+    });
+    expect(fetchStore.state.data).toEqual({
+      url: 'https://www.google.com.tw/',
+    });
+  });
+});
+
+describe('fetch store - actions', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it('fetchAPI', (done) => {
+    moxios.stubRequest('https://api.github.com/', {
+      status: 200,
+      response: {
+        data: {
+          url: 'https://www.google.com.tw/',
+        },
+      },
+    });
+
+    testAction(
+      fetchStore.actions.fetchAPI,
+      {
+        data: {
+          url: 'https://www.google.com.tw/',
+        },
+      },
+      {},
+      [
+        {
+          type: types.FETCH_API,
+          payload: {
+            data: {
+              url: 'https://www.google.com.tw/',
+            },
+          },
+        },
+      ],
+      done
+    );
+  });
+});
